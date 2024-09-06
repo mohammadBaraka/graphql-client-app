9e67e0ac9a453d2c625cd69aad5e16d028a9ab0d
mohammadBaraka <mohammadbarakat342@gmail.com>
2024-09-06 04:40:52 -0700

    fix debloying

--- app/components/GetPrams.jsx
+++ app/components/GetPrams.jsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { Loader } from "./Loader/Loader";
 const GetPrams = () => {
   const params = useSearchParams().get("post");
-  return params;
+  return <Suspense fallback={<Loader />}>{params}</Suspense>;
 };
 
 export default GetPrams;
