1cfcbe35dbf51e6d07adbd8ddd2752427879dfca
mohammadBaraka <mohammadbarakat342@gmail.com>
2024-09-06 04:43:01 -0700

    fix debloying searchparams

--- app/components/GetPrams.jsx
+++ app/components/GetPrams.jsx
@@ -5,6 +5,7 @@ import { Suspense } from "react";
 import { Loader } from "./Loader/Loader";
 const GetPrams = () => {
   const params = useSearchParams().get("post");
+  //   add a unique key prop if possible
   return <Suspense fallback={<Loader />}>{params}</Suspense>;
 };
 
